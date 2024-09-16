74aa1a5aa2303868df9da4896d800b87f03e4c22
Carlos Acosta <cjamcu@gmail.com>
2024-09-15 22:17:24 -0500

    change serve start message when  run app

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 	"os"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 	"github.com/pocketbase/pocketbase/tools/cron"
 )
 
 func main() {
-	app := pocketbase.New()
 
 	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	app := pocketbase.NewWithConfig(pocketbase.Config{
+		HideStartBanner: true,
+		DefaultDev:      isGoRun,
+	})
 
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
 		Automigrate: isGoRun,
@@ -27,7 +33,35 @@ func main() {
 	pb.SetupRoutes(app)
 	pb.SetupScheduler(app, scheduler)
 
+	// Setup output message
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		schema := "http"
+		addr := e.Server.Addr
+
+		if strings.Contains(addr, ":443") {
+			schema = "https"
+			args := os.Args
+			// Get domain from args and set addr to addr
+			if len(args) > 2 {
+				addr = args[2]
+			}
+		}
+
+		date := new(strings.Builder)
+		log.New(date, "", log.LstdFlags).Print()
+
+		bold := color.New(color.Bold).Add(color.FgGreen)
+		bold.Printf(
+			"%s Server started at %s\n",
+			strings.TrimSpace(date.String()),
+			color.CyanString("%s://%s", schema, addr),
+		)
+
+		return nil
+	})
+
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+
 }
